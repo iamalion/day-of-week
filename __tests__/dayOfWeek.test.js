6f7418df07fd762a415f3b080cb8e8b6d1e02313
Lindsay Warr <iamalion@gmail.com>
2023-07-11 14:54:10 -0700

    Commit uncommitted files

--- __tests__/dayOfWeek.test.js
+++ __tests__/dayOfWeek.test.js
@@ -4,6 +4,35 @@ describe('DayOfWeek', () => {
 
     test ('should create a day of week object with a date', () => {
         const dayOfWeek = new DayOfWeek("March 23, 1986");
-        expect(dayOfWeek.date).toBe("March 23, 1986");
+        expect(dayOfWeek.date.getDate()).toBe(23);
+        expect(dayOfWeek.date.getMonth()).toBe(2);
+        expect(dayOfWeek.date.getFullYear()).toBe(1986);
+
+    });
+
+    test ('should output day of the week depending on date',() => {
+        const dayOfWeek = new DayOfWeek("March 23, 1986");
+        expect(dayOfWeek.getWeekday()).toBe("Sunday");
+    });
+
+    test ('should output the string "invalid date"', () => {
+        const dayOfWeek = new DayOfWeek("Sept 14, 275760");
+        expect(dayOfWeek.date.getDate()).toBe(NaN);
+    });
+
+    test ('should create a day of week object with a date', () => {
+        const dayOfWeek = new DayOfWeek("Sept 12, 275760");
+        expect(dayOfWeek.date.getDate()).toBe(12);
+        expect(dayOfWeek.date.getMonth()).toBe(8);
+        expect(dayOfWeek.date.getFullYear()).toBe(275760);
+
+    });
+
+    test ('should create a day of week object with a date', () => {
+        const dayOfWeek = new DayOfWeek("April 21, -271821");
+        expect(dayOfWeek.date.getDate()).toBe(21);
+        expect(dayOfWeek.date.getMonth()).toBe(3);
+        expect(dayOfWeek.date.getFullYear()).toBe(271821);
+
     });
 })
--- src/dayOfWeek.js
+++ src/dayOfWeek.js
@@ -1,9 +1,33 @@
 export default class DayOfWeek {
     constructor(date){
-        this.date = date;
+        this.date = new Date(date)
+        console.log(typeof this.date)
     }
 
-    checkWeekday(){
-        
+    getWeekday(){
+        let dayOfWeek = this.date.getDay();
+        let dayString = ''
+        switch(dayOfWeek){
+            case 0:
+                dayString = "Sunday";
+                break;
+            case 1: 
+                dayString = "Monday";
+                break;
+            case 2:
+                dayString = "Tuesday";
+                break;
+            case 3:
+                dayString = "Wednesday";
+                break;
+            case 4:
+                dayString = "Thursday";
+                break;
+            case 5:
+                dayString = "Friday";
+                break;
+            case 6:
+                dayString = "Saturday";
+        } return dayString
     }
 }
